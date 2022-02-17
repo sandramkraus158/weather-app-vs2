ab26238977e21d951be1f8113d267568d9e2114a
sandramkraus158 <sandramkraus158@gmail.com>
2022-02-17 09:59:34 +0100

    Added wind direction

--- src/app.js
+++ src/app.js
@@ -65,6 +65,42 @@ function getForecast(coordinates) {
   axios.get(apiUrl).then(displayForecast);
 }
 
+function getWindDirection(direction) {
+  if (direction >= 348.75 || direction < 11.25) {
+    return "N";
+  } else if (direction >= 11.25 && direction < 33.75) {
+    return "NNE";
+  } else if (direction >= 33.75 && direction < 56.25) {
+    return "NE";
+  } else if (direction >= 56.25 && direction < 78.75) {
+    return "ENE";
+  } else if (direction >= 78.25 && direction < 101.25) {
+    return "E";
+  } else if (direction >= 101.25 && direction < 123.75) {
+    return "ESE";
+  } else if (direction >= 123.75 && direction < 146.25) {
+    return "SE";
+  } else if (direction >= 146.25 && direction < 168.75) {
+    return "SSE";
+  } else if (direction >= 168.75 && direction < 191.25) {
+    return "S";
+  } else if (direction >= 191.25 && direction < 213.75) {
+    return "SSW";
+  } else if (direction >= 213.75 && direction < 236.25) {
+    return "SW";
+  } else if (direction >= 236.25 && direction < 258.75) {
+    return "WSW";
+  } else if (direction >= 258.75 && direction < 281.25) {
+    return "W";
+  } else if (direction >= 281.25 && direction < 303.75) {
+    return "WNW";
+  } else if (direction >= 303.75 && direction < 326.25) {
+    return "NW";
+  } else if (direction >= 326.25 && direction < 348.75) {
+    return "NNW";
+  }
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -72,6 +108,7 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let windDirectionElement = document.querySelector("#wind-direction");
   let feelsElement = document.querySelector("#feels");
   let tempMaxElement = document.querySelector("#temp-max");
   let tempMinElement = document.querySelector("#temp-min");
@@ -85,7 +122,8 @@ function displayTemperature(response) {
   countryElement.innerHTML = response.data.sys.country;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = Math.round(response.data.main.humidity);
-  windElement.innerHTML = Math.round(response.data.wind.speed);
+  windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
+  windDirectionElement.innerHTML = getWindDirection(response.data.wind.deg);
   feelsElement.innerHTML = Math.round(response.data.main.feels_like);
   tempMaxElement.innerHTML = Math.round(response.data.main.temp_max);
   tempMinElement.innerHTML = Math.round(response.data.main.temp_min);
